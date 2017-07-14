380db0c41da20e1578b6dfc0a9da227692a408fe
hphilamore <hemma@Hemmas-MacBook-Pro.local>
2017-07-14 16:09:07 +0900

    using a timer and flags to do an operation after a set time period and continue doing it until interrupted, at which point the timer is reset

--- pedometer.ino
+++ pedometer.ino
@@ -1,107 +1,96 @@
-#include <Adafruit_NeoPixel.h>
+// I2C interface by default
+//
 #include "Wire.h"
 #include "SparkFunIMU.h"
 #include "SparkFunLSM303C.h"
 #include "LSM303CTypes.h"
+#include <Adafruit_NeoPixel.h>
 
 #define PIN 8
 #define NUM_LEDS 1
 
-//create a NeoPixel strip
 Adafruit_NeoPixel strip = Adafruit_NeoPixel(NUM_LEDS, PIN, NEO_GRB + NEO_KHZ800);
 
-//create accelerometer
+// #define DEBUG 1 in SparkFunLSM303C.h turns on debugging statements.
+// Redefine to 0 to turn them off.
+
 LSM303C myIMU;
 
-float x, y, z; 
-float threshold = 3;
-float thresholdPace = 3;
+float x; 
+float y; 
+float z; 
+float w;
+int steps, flag;// , flag_movement;
+float threshold = 1200;
+unsigned long startTime = millis();
+int interval = 10000;//600000;
+int flag_movement = 0;
 
-int steps, flag; //flag; // 
-long MeasurePeriod = 3000; 
-unsigned long startTime = millis() ;   
-unsigned long endTime = millis(); 
-int stepsOld = 0;
-int stepsNew; 
-float paceAve, timer, pacer; 
 
 void setup()
 {
   Serial.begin(115200);
-  strip.begin();
-  strip.show();
   if (myIMU.begin() != IMU_SUCCESS)
   {
     Serial.println("Failed setup.");
     while(1);
-  }  
+  }
+  strip.begin();
+  strip.show();
+  //pinMode(LED_BUILTIN, OUTPUT);
 }
 
 void loop()
 {
+Serial.print(flag_movement);
+Serial.print("\t");
+  
 x = float((myIMU.readAccelX()));
 y = float((myIMU.readAccelY()));
 z = float((myIMU.readAccelZ()));
+w = sqrt(sq(x)+sq(y)+sq(z));
 
-Serial.print(x);
-Serial.print("\t");
-Serial.print(y);
-Serial.print("\t");
-Serial.println(z);
 
-delay(100);
+  //Serial.print("\t");
+  //x = 1;
 
-//  if (w>threshold && flag==0)
-//  {
-//    steps=steps+1;
-//    
-//    flag=1;  
+//  Serial.print(myIMU.readAccelX(), 4);
+//Serial.print("\t");
 //
-//     // light ON
-//    strip.setPixelColor(0, 255, 0, 0);
-//    strip.show();
-//    delay(200);
-//    // light OFF
-//    strip.setPixelColor(0, 0, 0, 0);
-//    strip.show();       
-//  }
-//  
-//   else if (w > threshold && flag==1)
-//  {
-//    //do nothing 
-//  }
-//  
-//  if (w <threshold  && flag==1)
-//  {
-//    flag=0;
-//  }
-//
-// Serial.print("\t");
-// Serial.println(steps); 
-// delay(100);   
-
-//
-//
-//  // ********************************************
-//
-//   if ((millis() - startTime) > MeasurePeriod)
-//   {
-//     stepsNew = steps;   
-//     endTime = millis();     
-//     paceAve = float(stepsNew - stepsOld)/ float(endTime - startTime); 
-//     stepsOld = steps;
-//     startTime = millis();    
-//   }
-//
-//   Serial.print("\t");
-//   Serial.println(paceAve, 6); 
-//
-//   if (paceAve > thresholdPace)
-//   {
-//    strip.setPixelColor(0, 0, 255, 0);
-//    strip.show();
-//    delay(200);
-//   }  
+//  Serial.print(myIMU.readAccelY(), 4);
+//  Serial.print("\t");
  
- 
+ Serial.print(x);
+Serial.print("\t");
+ Serial.print(y);
+Serial.print("\t");
+ Serial.print(z);
+Serial.print("\t");
+  Serial.println(w);
+
+if(w<threshold)
+  {
+    if (flag_movement == 0)
+    {
+      if (millis() - startTime > interval)
+      {
+        strip.setPixelColor(0, 255, 0, 0);                 // HP : Curly braces around code after if statement
+        strip.show();
+       }
+     }
+  
+     else
+     {
+      flag_movement = 0;
+     }
+  }
+
+else
+  {
+    flag_movement = 1;
+    startTime = millis();
+    strip.setPixelColor(0, 0, 0, 0);                   // HP : Curly braces around code after if statement
+    strip.show(); // if the light turns on, the person is in danger.  // HP: Use // for comments
+  }
+  delay(100);
 }
